db6f26d8371628a7d8c57f4f4b7e1ab0f56e7309
dav_av <adavid0707@gmail.com>
2020-07-02 16:35:10 +0400

    css modulnerov sovorecinq ashxatel

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
+import classes from './Navbar.module.css'
+ // css petqa modulenrov grenq vor amen meki hamar orinak item class@ sax texer@ grac lini item bayc tarber linen petqa Navbar.css tex@ grel Navbar.module.css u import anel stex classes@ es voncvor taza object sarqem pahuma sax ira mej dursa galis senc
+/* 
+  let classes = {
+    "nav":"u stex ira parametrer@",
+    "item" : u stex orinak font-color:white
+  }
+  erb vor grum enq module es mer grac orinak nav class@ darnuma senc
+  Navbar_nav__365465465 orinak
+  u petqa grenq senc nayi tak@
+*/
 const Nav =()=>{
     return(
-        <nav className = "nav">
-        <div>
+        <nav className = {classes.nav}>
+        <div className = {classes.item}>
           <a>Profile</a>
         </div>
-        <div>
+        <div className = {classes.item}>
           <a>Messages</a>
         </div>
-        <div>
+        <div className = {classes.item}> 
           <a>News</a>
         </div>
-        <div>
+        <div className = {classes.item}>
           <a>Music</a>
         </div>
-        <div>
+        <div className = {classes.item}>
           <a>Settings</a>
         </div>
       </nav>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
-
+import p from './Profile.module.css';
 const Profile = ()=>{
     return(
-        <div className = "content">
+        <div className = {p.content}>
         <img src ="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTrKHPsvNDJHY9tWpkHrfkfo8Dkf0LvZU3Hdg&usqp=CAU"/>
       
-      <div>
+      <div >
         AVA +DESC
       </div>
       <div>
         MY POSTS
-        <div>
+        <div className ={p.posts}>
           NEW POST
         </div>
-        <div>
+        <div className ={p.item}>
           POST1
         </div>
-        <div>
+        <div className ={p.item}>
           POST 2
         </div>
       </div>
