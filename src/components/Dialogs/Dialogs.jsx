e1ae9d6d4c6d31185247186727e25ee247d2d91b
dav_av <adavid0707@gmail.com>
2020-07-04 21:04:35 +0400

    dialog@ ej@ tetev verstka

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,11 +3,32 @@ import s from './Dialogs.module.css'
 
 let Dialogs =(props) =>{
     return(
-        <div>
-            Dialogs
-            {/* <span className= {s.item}>sss</span> */}
+        <div className ={s.dialogs}> 
+            <div className ={s.dialogs_item}>
+                <div className = {s.dialog}>
+                    David
+                </div>
+                <div className = {s.dialog + ' ' + s.active}>
+                    Ani
+                </div>
+                <div className = {s.dialog}>
+                    John
+                </div>
+                <div className = {s.dialog}>
+                    EZO
+                </div>
+                <div className = {s.dialog}>
+                    Jennifer
+                </div>
+            </div>
+            <div className = {s.messages}>
+                <div className = {s.message}>HI</div>
+                <div className = {s.message}>Barev</div>
+                <div className = {s.message}>Privet</div>
+            </div>
         </div>
     )
 }
 
 export default Dialogs;
+console.log(s);
